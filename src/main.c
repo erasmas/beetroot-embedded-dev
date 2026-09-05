2e16314132654015692e1bd6fa4587d006ccf33c
Dmytro Kobza <active.book2771@fastmail.com>
2026-09-05 15:25:55 +0300

    chore: drop dependency on freertos

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include "calibration.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "light_sensor.h"
 
 static const char *TAG = "twilight";
